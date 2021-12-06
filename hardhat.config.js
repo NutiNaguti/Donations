5657912d5007101a695e5b4245fb601e85e1cce2
Danila <derzhavin.danila@icloud.com>
2021-12-06 12:19:18 +0400

    chaged config files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.10",
+  networks: {
+    hardhat: {
+      chainId: 1,
+    },
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/MB1erYtsiSKmEvN_tY7lNmiz4nFazqai",
+      accounts: ["d0ad863e2879c5f5ac0f4157deb2c12e3b0cb9ac5fa516544853120c3faac241"],
+    },
+  },
+  etherscan: {
+    apiKey: "MD92IUH2GSS1CB85AQIR51C8AWUDA4BY6X",
+  },
 };
